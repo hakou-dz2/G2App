4d57c4212bfd37ae14b712365a074f0dd6a1fd90
hakou_dz2 <hakou.silem@hotmail.com>
2025-04-23 15:03:49 +0100

    tablet

--- app/src/main/java/com/example/g2app/activities/SecondActivity.java
+++ app/src/main/java/com/example/g2app/activities/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.g2app.activities;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentController;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -28,22 +29,30 @@ public class SecondActivity extends AppCompatActivity implements CourseAdapter.O
 
     @Override
     public void onItemClick(Course course) {
-        FragmentTransaction fragmentController = getSupportFragmentManager().beginTransaction();
+        boolean Istablet = getResources().getBoolean(R.bool.IsTablet);
 
-        Bundle bundle = new Bundle();
-        bundle.putString("name",course.getName());
-        bundle.putDouble("grade",course.getGrade());
-        bundle.putDouble("credit",course.getCredit());
-        bundle.putDouble("coefficient",course.getCoefficient());
+        if(Istablet){
+            Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.tablet_frag_info);
+            InfoFragment info = (InfoFragment) fragment;
+            info.dispalyInfo(course);
+
+        }else {
+            FragmentTransaction fragmentController = getSupportFragmentManager().beginTransaction();
+
+            Bundle bundle = new Bundle();
+            bundle.putString("name", course.getName());
+            bundle.putDouble("grade", course.getGrade());
+            bundle.putDouble("credit", course.getCredit());
+            bundle.putDouble("coefficient", course.getCoefficient());
 
 
-        InfoFragment infoFragment = new InfoFragment();
-        infoFragment.setArguments(bundle);
+            InfoFragment infoFragment = new InfoFragment();
+            infoFragment.setArguments(bundle);
 
 
-        fragmentController.replace(R.id.frag_cont,infoFragment);
-        fragmentController.addToBackStack(null);
-        fragmentController.commit();
-
+            fragmentController.replace(R.id.frag_cont, infoFragment);
+            fragmentController.addToBackStack(null);
+            fragmentController.commit();
+        }
     }
 }
--- app/src/main/java/com/example/g2app/fragments/InfoFragment.java
+++ app/src/main/java/com/example/g2app/fragments/InfoFragment.java
@@ -12,10 +12,11 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.g2app.R;
+import com.example.g2app.struct.Course;
 
 
 public class InfoFragment extends Fragment {
-
+    TextView name,grade,credit,coefficient;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,23 +35,25 @@ public class InfoFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        TextView name = view.findViewById(R.id.tv_info_name);
-        TextView grade = view.findViewById(R.id.tv_info_grade);
-        TextView credit = view.findViewById(R.id.tv_info_cred);
-        TextView coefficient = view.findViewById(R.id.tv_info_coeff);
+         name = view.findViewById(R.id.tv_info_name);
+         grade = view.findViewById(R.id.tv_info_grade);
+         credit = view.findViewById(R.id.tv_info_cred);
+         coefficient = view.findViewById(R.id.tv_info_coeff);
 
         Bundle bundle = this.getArguments();
+        if(bundle != null) {
+            name.setText(bundle.getString("name"));
+            grade.setText(String.valueOf(bundle.getDouble("grade")));
+            credit.setText(String.valueOf(bundle.getDouble("credit")));
+            coefficient.setText(String.valueOf(bundle.getDouble("coefficient")));
+        }
+    }
 
-        name.setText(bundle.getString("name"));
-        grade.setText(String.valueOf(bundle.getDouble("grade")));
-        credit.setText(String.valueOf(bundle.getDouble("credit")));
-        coefficient.setText(String.valueOf(bundle.getDouble("coefficient")));
-
-
-
-
-
-
+    public void dispalyInfo(Course course){
+        name.setText(course.getName());
+        grade.setText(String.valueOf(course.getGrade()));
+        credit.setText(String.valueOf(course.getCredit()));
+        coefficient.setText(String.valueOf(course.getCoefficient()));
 
     }
 }
